284fff17188206c0cf763d16d821ea8c4edc61ba
dennis <denhouston1@icloud.com>
2022-11-13 19:19:19 +0000

    added auth and user routes to API, editied app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
-
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
 
@@ -16,6 +17,10 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, () => {
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to homepage")
