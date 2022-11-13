f54e8d321fd3b6f39485610d185e6629c015f629
dennis <denhouston1@icloud.com>
2022-11-13 19:10:39 +0000

    Added a bit late but moved env to gitignore

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, () => {
   console.log("Connected to MONGO")
 })
 
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage")
+})
+
+app.get("/users", (req, res) => {
+  res.send("Welcome to users page")
+})
+
 app.listen(port, () => {
   console.log(`Backend server is running on port ${port}!`)
 })
