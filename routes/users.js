889495a69e34de57aa8a601f74defade46beb4e1
dennis <denhouston1@icloud.com>
2022-11-22 15:58:26 +0000

    Completed update user

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt")
 
 //update user
 router.put("/:id", async (req, res) => {
-  if(req.body.userId === req.params.id || req.user.isAdmin){
+  if(req.body.userId === req.params.id || req.body.isAdmin){
     if(req.body.password){
       try{
         const salt = await bcrypt.genSalt(10);
@@ -25,10 +25,9 @@ router.put("/:id", async (req, res) => {
     return res.status(403).json("you can only update your account")
   }
 })
-//delete user
-//get a user
-//follow a user
-//unfollow a user
+
+
+
 
 
 module.exports = router
