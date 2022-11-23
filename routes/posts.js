504daac5267aaa41426691e3aa7c4eca943da819
dennis <denhouston1@icloud.com>
2022-11-23 10:32:15 +0000

    Added update post

--- routes/posts.js
+++ routes/posts.js
@@ -13,6 +13,19 @@ router.post('/', async (req, res) => {
 })
 
 // Update a post
+router.put('/:id', async (req, res) => {
+  try{
+    const post = await Post.findById(req.params.id)
+    if(post.userId === req.body.userId){
+      await post.updateOne({$set:req.body})
+      res.json(200).json("Post has been updated")
+    } else {
+      res.status(403).json("You can only update your post")
+    }
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
 
 // Delete a post
 // Like a post
