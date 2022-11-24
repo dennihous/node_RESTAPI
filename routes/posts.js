0f94b614a94c291b4aa1ebc56bec11420d3f6218
dennis <denhouston1@icloud.com>
2022-11-24 16:36:00 +0000

    Added posts get user post and get a users post

--- routes/posts.js
+++ routes/posts.js
@@ -28,9 +28,62 @@ router.put('/:id', async (req, res) => {
 })
 
 // Delete a post
-// Like a post
+router.delete('/:id', async (req, res) => {
+  try{
+    const post = await Post.findById(req.params.id)
+    if(post.userId === req.body.userId){
+      await post.deleteOne()
+      res.status(200).json("Post has been deleted")
+    } else {
+      res.status(403).json("You can only delete your post")
+    }
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+// Like and dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if(!post.likes.includes(req.body.userId)){
+      await post.updateOne({ $push: { likes: req.body.userId } })
+      res.status(200).json("Post has been liked")
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } })
+      res.status(200).json("Post has been disliked")
+    }
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
 // Get a post
+router.get("/:id", async (req, res) => {
+  try{
+    const post = await Post.findById(req.params.id)
+    res.status(200).json(post)
+  } catch(err){
+    res.status(500).json(err)
+  }
+})
+
 // Get of a users posts
+router.get("/timeline", async(req, res) => {
+  let postArray = []
+  try{
+    const currentUser = await User.findById(req.body.userId)
+    const userPosts = await Post.find({userId: currentUser._id })
+    const friendsPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+          Post.find({ userId: friendId })
+      })
+    )
+    res.json(userPosts.concat(...friendsPosts))
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
 
 
 module.exports = router
