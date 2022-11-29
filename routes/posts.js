454ce60581c8872d13802eaf4958951ce841e67e
dennis <denhouston1@icloud.com>
2022-11-29 12:55:30 +0000

    API complete

--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,4 @@
+const User = require("../models/User")
 const router = require("express").Router()
 const Post = require("../models/Post")
 
@@ -68,22 +69,21 @@ router.get("/:id", async (req, res) => {
   }
 })
 
-// // Get of a users posts
-// router.get("/timeline", async(req, res) => {
-//   let postArray = []
-//   try{
-//     const currentUser = await User.findById(req.body.userId)
-//     const userPosts = await Post.find({userId: currentUser._id })
-//     const friendsPosts = await Promise.all(
-//       currentUser.followings.map((friendId) => {
-//           Post.find({ userId: friendId })
-//       })
-//     )
-//     res.json(userPosts.concat(...friendsPosts))
-//   } catch(err) {
-//     res.status(500).json(err)
-//   }
-// })
+// // Get timeline posts
+router.get("/timeline/all", async(req, res) => {
+  try{
+    const currentUser = await User.findById(req.body.userId)
+    const userPosts = await Post.find({userId: currentUser._id })
+    const friendsPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+          return Post.find({ userId: friendId })
+      })
+    )
+    res.json(userPosts.concat(...friendsPosts))
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
 
 
 module.exports = router
