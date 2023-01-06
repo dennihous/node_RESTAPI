ff6bb01b28cd93747eb727e8dfe09be19c5ce482
dennis <denhouston1@icloud.com>
2023-01-06 08:27:06 +0000

    Added new get request

--- routes/posts.js
+++ routes/posts.js
@@ -69,7 +69,23 @@ router.get("/:id", async (req, res) => {
   }
 })
 
-// // Get timeline posts
+// Get timeline posts
+router.get("/timeline/:userId", async(req, res) => {
+  try{
+    const currentUser = await User.findById(req.params.userId)
+    const userPosts = await Post.find({userId: currentUser._id })
+    const friendsPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+          return Post.find({ userId: friendId })
+      })
+    )
+    res.status(200).json(userPosts.concat(...friendsPosts))
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+// Get timeline posts
 router.get("/timeline/:userId", async(req, res) => {
   try{
     const currentUser = await User.findById(req.params.userId)
